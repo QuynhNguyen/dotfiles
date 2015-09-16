e54d43af8ffa2a04a518abdecad6230f7f15fb73
Quynh Nguyen <quynhnguyen003@gmail.com>
2015-09-16 14:43:54 -0700

    update env and add submodule cocoapod update

--- system/env.zsh
+++ system/env.zsh
@@ -1,6 +1,2 @@
-editorSymlinkName=/usr/local/bin/editor
-if [ ! \( -L "${editorSymlinkName}" \) ]
-then
-	ln -s "/Applications/Visual Studio Code.app/Contents/MacOS/Atom" $editorSymlinkName
-fi
-export EDITOR=/usr/local/bin/editor
+export VISUAL=vim
+export EDITOR="$VISUAL"
