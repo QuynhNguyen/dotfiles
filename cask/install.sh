08874092f6096398c188b580edf7963efc84b1a3
Quynh Nguyen <quynhnguyen003@gmail.com>
2019-02-05 15:53:06 -0800

    add slack and vscode to cask

--- cask/install.sh
+++ cask/install.sh
@@ -1,6 +1,10 @@
+brew update 
+brew tap caskroom/cask
 brew cask install google-chrome
 brew cask install evernote
 brew cask install spotify
 brew cask install skype
 brew cask install messenger
 brew cask install dropbox
+brew cask install slack
+brew cask install visual-studio-code
